8d3487429ea3caa1f9a67c0e077a97de87cb6c4d
twój login do GitHuba <twj email>
2023-03-14 15:57:51 +0100

    Added logic to secondary functionality - WODs

--- src/components/WODs.jsx
+++ src/components/WODs.jsx
@@ -1,27 +1,81 @@
 import supabase from "../services/supabase";
 import { useEffect, useState } from "react";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+import { Scrollbars } from "react-custom-scrollbars";
 
 const WODs = () => {
+  const [selectedExercise, setSelectedExercise] = useState(null);
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    const fetchExercises = async () => {
+      const { data: exercises, error } = await supabase
+        .from("wods")
+        .select("*");
+
+      if (error) console.log("Error fetching exercises:", error);
+      else setExercises(exercises);
+    };
+
+    fetchExercises();
+  }, []);
+
   return (
     <div className="wods--main__container">
       <div className="selection--container">
         <h1 className="WODs--header">WORK OF THE DAY</h1>
-        <button className="choose--wods__button">Choose your WOD </button>
+        <Popup
+          trigger={
+            <button className="choose--wods__button">Choose your WOD </button>
+          }
+          position="right center"
+        >
+          {(close) => (
+            <div>
+              <ul>
+                {exercises.map((exercise) => (
+                  <li key={exercise.id}>
+                    <a
+                      href="#"
+                      onClick={() => {
+                        setSelectedExercise(exercise);
+                        close();
+                      }}
+                    >
+                      {exercise.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <a className="close" onClick={close}>
+                &times;
+              </a>
+            </div>
+          )}
+        </Popup>
       </div>
       <div className="wod--container">
-        <div className="number--of__wod">Benchmark #1</div>
-        <h1 className="wods--container__header">Cindy</h1>
-        <div className="wod--card__container">
-          <p className="work--of--the__day">
-            AMRAP in 20 minutes : 5 Pull-Ups, 10 Push-Ups, 15 Air Squats
-          </p>
-          <p className="wod--description">
-            How do you perform the “Cindy” workout? On a 20-minute clock,
-            perform as many rounds and reps as possible (AMRAP) of the work in
-            the order written. 1 round is equal to 5 Pull-Ups, 10 Push-Ups, and
-            15 Air Squats.
-          </p>
-        </div>
+        <Scrollbars style={{ width: "100%", height: "100%" }}>
+          <div className="number--of__wod">
+            {selectedExercise && selectedExercise.number_of_wod}
+          </div>
+          <h1 className="wods--container__header">
+            {selectedExercise
+              ? selectedExercise.name
+              : "Here you can see your today WORK OF THE DAY"}
+          </h1>
+          <div className="wod--card__container">
+            {selectedExercise && (
+              <>
+                <p className="work--of--the__day">{selectedExercise.wod}</p>
+                <p className="wod--description">
+                  {selectedExercise.description}
+                </p>
+              </>
+            )}
+          </div>
+        </Scrollbars>
       </div>
     </div>
   );
